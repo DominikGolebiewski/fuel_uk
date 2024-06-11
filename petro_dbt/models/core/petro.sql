d46895648c30c7ee4881a2504b532842753a53cb
Dominik Golebiewski <dominik.golebiewski@atheon.co.uk>
2024-06-11 10:10:46 +0100

    Update tests. Update db

--- petro_dbt/models/core/petro.sql
+++ petro_dbt/models/core/petro.sql
@@ -1,18 +1,25 @@
-select 
-    retailer,
-    brand,
-    address,
-    postcode,
-    location_latitude as latitude,
-    location_longitude as longitude,
-    "prices_B7" as b7,
-    "prices_E10" as e10,
-    "prices_E5" as e5,
-    "prices_SDV" as sdv,
-    last_updated,
-    {{ dbt_utils.generate_surrogate_key(['retailer', 'last_updated']) }} as surrogate_key
-from {{ ref('stg_petro') }}
+with petro_data as (
 
-{% if is_incremental() %}
-where last_updated > (select max(last_updated) from {{ this }})
-{% endif %}
+        select 
+            retailer,
+            brand,
+            address,
+            postcode,
+            location_latitude as latitude,
+            location_longitude as longitude,
+            "prices_B7" as b7,
+            "prices_E10" as e10,
+            "prices_E5" as e5,
+            "prices_SDV" as sdv,
+            last_updated,
+            -- row_number() over(partition by retailer, brand, strftime(DATE last_updated, '%d/%m/%Y'), address, postcode order by last_updated desc) as row_num,
+            {{ dbt_utils.generate_surrogate_key(['retailer', 'brand', 'address', 'last_updated']) }} as surrogate_key
+        from {{ ref('stg_petro') }}
+
+        {% if is_incremental() %}
+        where last_updated > (select max(last_updated) from {{ this }})
+        {% endif %}
+
+)
+select *
+from petro_data
